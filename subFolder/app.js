90b75c1941c0944936591977ee5238778e1c3318
mugendiPius <munenepeter910@gmail.com>
2025-03-04 11:54:11 +0300

    committion

--- subFolder/app.js
+++ subFolder/app.js
@@ -12,3 +12,18 @@ window.addEventListener('load',()=>{
 
     })
 })
+
+const btnHideAboutText=document.querySelector('.readMore')
+btnHideAboutText.addEventListener('click',(e)=>{
+    e.preventDefault();
+    let aboutTextToHide=document.querySelector('.about_text span');
+    let aboutMeContainer= document.querySelector('.aboutMeWrapper');
+
+    aboutTextToHide.classList.toggle('hideAboutText');
+    if(aboutTextToHide.classList.contains('hideAboutText')){
+        aboutMeContainer.style='max-height: 40vh;';
+        btnHideAboutText.textContent='Read More.....';
+    }else{
+        btnHideAboutText.textContent='See Less....';
+    }
+})
